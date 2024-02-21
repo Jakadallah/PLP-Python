557f67c88bc4413184be9590c0d5863a276c5339
Jakadallah <nashonomondi010@gmail.com>
2024-02-22 00:15:56 +0300

    Pyhton First code

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input('What is your name?  ')
+age = input('How old are you? ')
+location =input ('Where are you located? ')
+u = 'Hello  ' + name + '  you are  ' + age + '  years old and live in  ' + location
+print(u)
